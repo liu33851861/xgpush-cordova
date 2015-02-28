641276d74c015cd35223629f82d8e4635302a6f8
Gen Liu <gg@linux-o4mm.site>
2015-02-28 17:22:23 +0800

    change js module

--- www/xgpush.js
+++ www/xgpush.js
@@ -1,15 +1,32 @@
-/*
+/**
  * xpush cordova plugin
  * @author gg
- * @date 2014-03-21
+ * @date 2015-02-27
  */
 var exec = require('cordova/exec');
-
-xgpush.prototype.registerPush = function () {
-};
-
 
 module.exports = new xgpush();
 
 function xgpush() {
+}
+
+xgpush.prototype.registerPush = function (account, callback) {
+
+  exec(function (result) {
+    callback(null, result.title, result.content, result.customContent);
+
+  }, function () {
+    callback('error');
+
+  }, 'XgPush', 'register_push', [account]);
+
+};
+
+xgpush.prototype.unregisterPush = function () {
+
+  exec(function () {
+
+  }, function () {
+
+  }, 'XgPush', 'unregister_push', []);
 }
